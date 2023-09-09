b250fcdbaf849e060bc55af58bf0354ee6a091eb
Abhinav kumar <87375069+Abhinav-2208@users.noreply.github.com>
2023-09-09 18:32:12 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,87 @@
+# from django.shortcuts import render
+# from django.contrib import messages
+# import requests
+# import datetime
+
+# # Create your views here.
+# def home(request):
+#     if 'city' in request.POST:
+#         city = request.POST['city']
+#     else:
+#         city = 'Raipur'
+    
+#     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=73e962072af8a854d71a7b6d49dbbfe2'
+#     PARAMS = {'units': 'metric'}
+    
+#     try:
+#         data = requests.get(url,PARAMS).json()
+#         if 'weather' in data:
+#            description = data['weather'][0]['description']
+#            icon = data['weather'][0]['icon']
+#         else:
+#            description = "Weather data not available"
+#            icon = ""
+
+#         temp = data['main']['temp']
+    
+#         day = datetime.date.today()
+
+#         return render(request, 'app/home.html', {'description': description, 'icon': icon, 'temp': temp, 'day': day,'city':city})
+#     except:
+#       exception_occured=True
+#       messages.error(request,'Entered Data is not available to API')
+#       day=datetime.date.today()
+
+
+
+# start from here..
+from django.shortcuts import render
+from django.contrib import messages
+import requests
+import datetime
+
+
+def home(request):
+   
+    if 'city' in request.POST:
+         city = request.POST['city']
+    else:
+         city = 'Raipur'     
+    
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=73e962072af8a854d71a7b6d49dbbfe2'
+    PARAMS = {'units':'metric'}
+
+    API_KEY =  'AIzaSyCIug9AA3iySWnCsGnHqSmIqHicac-M8tE'
+
+    SEARCH_ENGINE_ID = 'f22bd23ab4a6a448c'
+     
+    query = city + " 1920x1080"
+    page = 1
+    start = (page - 1) * 10 + 1
+    searchType = 'image'
+    city_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
+
+    data = requests.get(city_url).json()
+    # count = 1
+    search_items = data.get("items")
+    image_url = search_items[1]['link']
+    
+
+    try:
+          
+          data = requests.get(url,params=PARAMS).json()
+          description = data['weather'][0]['description']
+          icon = data['weather'][0]['icon']
+          temp = data['main']['temp']
+          day = datetime.date.today()
+
+          return render(request,'app/home.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city , 'exception_occurred':False ,'image_url':image_url})
+    
+    except KeyError:
+          exception_occurred = True
+          messages.error(request,'Entered data is not available to API')   
+          
+          day = datetime.date.today()
+
+          return render(request,'app/home.html' ,{'description':'clear sky', 'icon':'01d'  ,'temp':25 , 'day':day , 'city':'indore' , 'exception_occurred':exception_occurred } )
+               
